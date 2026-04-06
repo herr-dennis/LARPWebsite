e6bac7a3d99b92614e028476df4f8398ab0975ab
Dennis Schwarz <118225088+herr-dennis@users.noreply.github.com>
2026-04-07 00:18:39 +0200

    05.4.26 fix / View

--- resources/views/galleryView.blade.php
+++ resources/views/galleryView.blade.php
@@ -8,9 +8,23 @@
 
     <div class="defaultContainer" >
         <p>
-            Stöbere, viele Fotos sind von Fotografen, lasst Anerkennung da, und besucht ihr SocialMedia Accounts
+            Viele der hier gezeigten Bilder wurden von engagierten Fotografen festgehalten, die unsere Erlebnisse und Geschichten in beeindruckenden Momentaufnahmen verewigt haben.
+
+            Zollt ihnen die verdiente Anerkennung und besucht ihre Social-Media-Kanäle. Jeder Blick, jedes Like und jedes Wort der Wertschätzung unterstützt ihre Arbeit und hält diese Erinnerungen lebendig.
         </p>
     </div>
+
+    <ul class="socialContainer" >
+        <li class="socialContainer__li" >  <a  target="_blank" href="https://www.instagram.com/aloha_airbrush_aloha_pictures?igsh=MWl5aTg1NnM5NDhocQ==" > aloha_airbrush_aloha_pictures</a></li>
+        <li class="socialContainer__li" ><a  target="_blank" href="https://www.instagram.com/koboldfoto?igsh=MXZmcjdwOTB5dTc2Nw==" >Kobold Foto</a></li>
+        <li class="socialContainer__li" ><a  target="_blank" href="https://www.instagram.com/hagen.photographer?igsh=MTAwdHViNnN0M2Z3bQ==" >Hagen Photographer</a></li>
+        <li class="socialContainer__li" ><a  target="_blank" href="https://www.instagram.com/the_art_of_larp?igsh=ZGR3YWRiZjJic2xu" >The Art of Larp</a></li>
+        <li class="socialContainer__li" ><a  target="_blank" href="https://www.instagram.com/ibensina_adi?utm_source=qr&igsh=MTJhaXNxMnhkampzZQ==" > Adi Ibensina</a></li>
+
+    </ul>
+
+
+
     <div id="app" data-page="medienGallery"></div>
 @endsection
 
